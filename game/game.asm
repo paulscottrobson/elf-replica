3b824aa7b814167a0f0d80ac298ba893c9be61b9
Paul Robson <paulscottrobson@gmail.com>
2016-06-30 16:49:28 +0100

    Maze Generation Working

--- game/game.asm
+++ game/game.asm
@@ -6,28 +6,32 @@ map = 		0E00h 																; this page has the map in it.
 stack = 	0DFFh 																; stack top
 
 	ret 																		; 1802 interrupts on. 
+	nop
 	lri 	r1,Interrupt 														; set interrupt vector
 	lri 	r2,Stack 															; set stack address
 	ldi 	Main & 255 															; switch to R3 as program pointer
 	plo 	r3
 	sep 	r3 																	; go to main routine
 Main:
-
-Next:
-	call 	r4,RandomNumber
-	br 		Next
-
 	sex 	r2 																	; turn video on
 	inp		1
+
+	call 	r4,CreateMaze
+
+Repaint:
 	call 	r4,RepaintDisplay 													; clear screen and draw walls
 	; Open doors
 	call 	r4,MirrorDisplay 													; mirror top of display to bottom
 	; draw princess
 	; draw status.
 
+	inc 	r8
+	br 		Repaint
+
 wait:
 	br 		wait
 
+	org 	100h
 	include interrupt.asm														; screen driver
 	include repaint.asm 														; repaint outline and mirror code
 	include maze.asm 															; maze creator.
--- game/maze.asm
+++ game/maze.asm
@@ -1,5 +1,70 @@
 
 
+; ************************************************************************************************************
+; ************************************************************************************************************
+;
+;												Maze Creator
+;
+; ************************************************************************************************************
+; ************************************************************************************************************
+
+MAZE_Wall = 080h
+MAZE_Open = 000h
+MAZE_Princess = 001h
+
+CreateMaze:
+	sex 	r2
+	lri 	rc,map 																; point RC to the map.
+__CMFill:
+	ldi 	MAZE_Wall 													 		; fill the map with solid walls.
+	str 	rc
+	inc 	rc
+	glo 	rc
+	bnz 	__CMFill
+	lri 	rc,map+16+1 														; row 1 column 1
+	lri 	r5,RandomNumber 													; r5 = random subroutine (runs in R4)
+__CMGenerate:
+	glo 	rc 																	; do not overright right hand wall
+	ani 	0Fh 
+	xri 	0Fh
+	bz 		__CMNext
+
+	glo 	rc  																; makes the checkerboard pattern.
+	ani	 	011h
+	xri 	011h 																
+	bnz 	__CMNext
+	ldi 	MAZE_Open
+	str 	rc 																	; write 00 (maze open) here.
+
+	recall 	r5																	; random number
+	ani 	15
+	smi 	11 																	; check mod 16 < 11
+	bdf 	__CMNotRight
+	inc 	rc 																	; if so open square to right.
+	ldi 	MAZE_Open
+	str 	rc
+	dec 	rc
+__CMNotRight:
+
+	recall 	r5 																	; random number
+	ani 	15
+	smi 	11 			 														; check mod 16 < 11
+	bdf 	__CMNotDown
+	ghi 	rc 																	; open square down - point RD to it
+	phi 	rd
+	glo 	rc
+	adi 	16
+	plo 	rd
+	ldi 	MAZE_Open 															; open that square
+	str 	rd
+__CMNotDown:
+
+__CMNext:
+	inc 	rc 																	; next
+	glo 	rc
+	xri 	0DFh 																; reached bottom right ?
+	bnz 	__CMGenerate
+	return
 
 ; ************************************************************************************************************
 ; ************************************************************************************************************
@@ -16,7 +81,7 @@ __RNHighM1:
 	ldi 	0ACh																; get seeded value
 	shr 																		; shift it right
 	str 	rf 																	; write it back
-	ldi 	__RNLowM1+1 														; change pointer
+	ldi 	(__RNLowM1+1) & 0FFh 												; change pointer
 	plo 	rf
 __RNLowM1:
 	ldi 	0E1h 																; get upper seeded value
@@ -26,7 +91,7 @@ __RNLowM1:
 	str 	r2
 	bnf  	__RNNoToggle 														; if bit shifted out set
 
-	ldi 	__RNHighM1+1														; exor the high bit with $B4
+	ldi 	(__RNHighM1+1) & 0FFh												; exor the high bit with $B4
 	plo 	rf
 	ldn 	rf
 	xri 	0B4h
