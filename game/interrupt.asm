7f22bba61ebaae8b4c28dfe39d6cc51f8c2ebcd3
Paul Robson <paulscottrobson@gmail.com>
2016-07-01 07:11:30 +0100

    Updates

--- game/interrupt.asm
+++ game/interrupt.asm
@@ -10,14 +10,17 @@ Return:
 	ldxa 																		; restore D
 	ret 																		; restore X,P
 Interrupt:
-	dec 	r2 																	; save return XP on stack
-	sav
-	dec 	r2 																	; save D on stack
-	str 	r2
+	dec 	r2 																	; [2/2] save return XP on stack 
+	sav 																		; [2/4]
+	dec 	r2 																	; [2/6] save D on stack
+	str 	r2																	; [2/8]
+	ldi 	display/256 														; [2/10] set up R0.1
+	phi 	r0 																	; [2/12]
+	ldi 	0 																	; [2/14] set up R0.0
+	plo 	r0
 	nop 																		; pad out cycles till rendering
 	nop
 	nop
-	lri 	r0,display 															; draw from here
 Refresh:
 	glo 	r0 																	; do four scan lines for each row
 	sex 	r2
